3a0d2d7014b1e19903b070a38bfa6d3861ff38e3
MGIC12 <mager.infante@gmail.com>
2024-10-23 21:36:16 -0300

    Conexión a database usando Sequelize realizada

--- config/db.js
+++ config/db.js
@@ -1,1 +1,14 @@
 const { Sequelize } = require("sequelize");
+
+const sequelize = new Sequelize("to-do-list", "root", "", {
+  dialect: "mysql",
+  port: 3306,
+  host: "localhost",
+});
+
+try {
+  sequelize.authenticate();
+  console.log("Connection has been established successfully.");
+} catch (error) {
+  console.error("Unable to connect to the database:", error);
+}
