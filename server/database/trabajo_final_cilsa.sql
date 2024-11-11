8c0931efaa9ed7b2aa631719438d323dbb651f0f
MGIC12 <mager.infante@gmail.com>
2024-11-11 15:24:13 -0300

    Archivo SQL con tablas correspondientes agregado

---
+++ server/database/trabajo_final_cilsa.sql
@@ -0,0 +1,21 @@
+/* TABLA USERS */
+CREATE TABLE users (
+    idUsuario int(50) NOT NULL AUTO_INCREMENT,
+    usuario varchar(50) NOT NULL,
+    nombre varchar(100) NOT NULL,
+    email varchar(255) NOT NULL,
+    password varchar(255) NOT NULL,
+    PRIMARY KEY (idUsuario)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci
+
+/* TABLA TAREAS */
+CREATE TABLE tareas (
+    idTarea int(11) NOT NULL AUTO_INCREMENT,
+    idUsuario int(11) NOT NULL,
+    fechaCreacion datetime NOT NULL,
+    estado tinyint(1) NOT NULL,
+    descripcion varchar(255) NOT NULL,
+    PRIMARY KEY (idTarea),
+    KEY idUsuario (idUsuario),
+    CONSTRAINT tareas_ibfk_1 FOREIGN KEY (idUsuario) REFERENCES users (idUsuario)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci
