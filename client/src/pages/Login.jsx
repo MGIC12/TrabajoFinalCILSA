b75c557d773c53392b748ad03e99b2e7507c78f2
MGIC12 <mager.infante@gmail.com>
2024-11-15 12:09:53 -0300

    Pequeños cambios

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,56 +1,61 @@
-import { useState } from 'react';
-import axios from 'axios';
-import { useNavigate, Link } from 'react-router-dom';
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate, Link } from "react-router-dom";
 
 export const Login = () => {
-  const [ email, setEmail ] = useState('');
-  const [ password, setPassword ] = useState('');
-  const [ error, setError ] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
   const navigate = useNavigate(); // Para redireccionar
 
   const handleLogin = (e) => {
     e.preventDefault();
 
-    axios.post('http://localhost:3001/login', { email, password })
-    .then((response) => {
-      const idUsuario = response.data.id;
-      navigate(`/todo/${idUsuario}`); // Redirige al usuario a la pagina con sus tareas.
-    })
-    .catch((error) => {
-      if (error.response && error.response.status === 401) {
-        setError('Credenciales incorrectas');
-      } else if (error.response.status === 400){
-        setError('Faltan credenciales');
-      } else {
-        setError('Error en el servidor');
-      }
-    })
-  }
+    axios
+      .post("http://localhost:3001/login", { email, password })
+      .then((response) => {
+        const idUsuario = response.data.id;
+        navigate(`/todo/${idUsuario}`); // Redirige al usuario a la pagina con sus tareas.
+      })
+      .catch((error) => {
+        if (error.response && error.response.status === 401) {
+          setError("Credenciales incorrectas");
+        } else if (error.response.status === 400) {
+          setError("Faltan credenciales");
+        } else {
+          setError("Error en el servidor");
+        }
+      });
+  };
 
   return (
     <section className="center">
       <h2>LOGIN</h2>
       <form onSubmit={handleLogin} className="form">
         <label>CORREO ELECTRONICO</label>
-        <input 
-        id="" 
-        placeholder="Correo electronico" 
-        type="text" 
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}/>
+        <input
+          id=""
+          placeholder="Correo electronico"
+          type="text"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
 
         <label>CONTRASEÑA</label>
         <input
-        id="" 
-        placeholder="Contraseña" 
-        type="text" 
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}/>
+          id=""
+          placeholder="Contraseña"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
 
         <button type="submit">Ingresar</button>
       </form>
-      {error && <p style={{ color: 'red' }}>{error}</p>}
-      <p>No tenes cuenta? <Link to='/register'>Registrate</Link></p>
+      {error && <p style={{ color: "red" }}>{error}</p>}
+      <p>
+        No tenes cuenta? <Link to="/register">Registrate</Link>
+      </p>
     </section>
-  )
-}
+  );
+};
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,59 +1,62 @@
 import { useState } from "react";
-import axios from 'axios';
+import { Link } from "react-router-dom";
+import axios from "axios";
 
 export const Register = () => {
-  const [nombre, setNombre] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [mensaje, setMensaje] = useState('');
+  const [nombre, setNombre] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [mensaje, setMensaje] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault(); // Evita que el formulario se envíe de la forma tradicional
 
     // Realiza la solicitud POST
-    axios.post('http://localhost:3001/crearUser', { nombre, email, password })
-    .then((response) => {
-      setNombre(''); // Limpia el formulario
-      setEmail('');
-      setPassword('');
-      setMensaje(response.data.message); // Muestra un mensaje de éxito
-    })
-  }
-
+    axios
+      .post("http://localhost:3001/crearUser", { nombre, email, password })
+      .then((response) => {
+        setNombre(""); // Limpia el formulario
+        setEmail("");
+        setPassword("");
+        setMensaje(response.data.message); // Muestra un mensaje de éxito
+      });
+  };
 
   return (
     <section className="center">
       <h2>REGISTER</h2>
       <form onSubmit={handleSubmit} className="form">
-
         <label>NOMBRE</label>
-        <input 
-        id="" 
-        placeholder="su nombre"
-        type="text" 
-        value={nombre}
-        onChange={(e) => setNombre(e.target.value)} />
+        <input
+          id=""
+          placeholder="su nombre"
+          type="text"
+          value={nombre}
+          onChange={(e) => setNombre(e.target.value)}
+        />
 
         <label>EMAIL</label>
-        <input 
-        id="" 
-        placeholder="email" 
-        type="text" 
-        value={email}
-        onChange={(e) => setEmail(e.target.value)} />
+        <input
+          id=""
+          placeholder="email"
+          type="text"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
 
         <label>CONTRASEÑA</label>
-        <input 
-        id="" 
-        placeholder="contraseña"
-        type="text" 
-        value={password}
-        onChange={(e) => setPassword(e.target.value)} />
+        <input
+          id=""
+          placeholder="contraseña"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
 
         <button type="submit">REGISTRARSE</button>
       </form>
       {mensaje && <p>{mensaje}</p>}
-      {mensaje && <Link to='/login'>Iniciá sesión</Link>}
+      {mensaje && <Link to="/login">Iniciá sesión</Link>}
     </section>
-  )
-}
+  );
+};
